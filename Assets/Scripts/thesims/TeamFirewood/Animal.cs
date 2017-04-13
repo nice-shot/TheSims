f55e039522f8c64ee6b272d8beb68249610004ac
nice-shot <shefferu@gmail.com>
2017-04-13 12:16:08 +0300

    added an animal script so that animal now moves around. Need to make it move in a smart way and to make it dissapear if it's captured

---
+++ Assets/Scripts/thesims/TeamFirewood/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamFirewood {
+public class Animal : MonoBehaviour {
+    [SerializeField] float moveSpeed = 1f;
+    	
+	// Randomly moves somewhere
+	void Update () {
+        var pos = gameObject.transform.position;
+        pos.x += Random.Range(0f, moveSpeed * Time.deltaTime);
+        pos.y += Random.Range(0f, moveSpeed * Time.deltaTime);
+        gameObject.transform.position = pos;
+	}
+}
+}
