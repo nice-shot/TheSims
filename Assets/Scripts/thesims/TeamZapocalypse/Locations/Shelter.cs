c6ca980513dd3db242747d2134974c2f431f3203
Nadav Sheffer <shefferu@gmail.com>
2017-04-13 20:24:15 +0300

    Added the shield mechanic with decreasing energy

--- Assets/Scripts/thesims/TeamZapocalypse/Locations/Shelter.cs
+++ Assets/Scripts/thesims/TeamZapocalypse/Locations/Shelter.cs
@@ -3,6 +3,29 @@ using Ai.Goap;
 
 namespace TeamZapocalypse {
 public class Shelter : MonoBehaviour, IStateful {
+
+    public float shieldEnergy = 60f;
+    public GameObject shield;
+
+    private const float energyDeplitionRate = 0.2f;
+
+    protected void Awake() {
+        CheckShield();
+    }
+
+    protected void Update() {
+        shieldEnergy = Mathf.Max(0, shieldEnergy - Time.deltaTime * energyDeplitionRate);
+        CheckShield();
+    }
+
+    private void CheckShield() {
+        if (shieldEnergy > 10f) {
+            shield.SetActive(true);
+        } else {
+            shield.SetActive(false);
+        }
+    }
+
     public State GetState() {
         // TODO: Add limited resources
         return null;
