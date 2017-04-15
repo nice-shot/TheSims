050fcadd35bff430106ded1774db352a45531c6e
Nadav Sheffer <shefferu@gmail.com>
2017-04-15 19:57:31 +0300

    Added the ability to shoot bullets

--- Assets/Scripts/thesims/TeamZapocalypse/Characters/Player.cs
+++ Assets/Scripts/thesims/TeamZapocalypse/Characters/Player.cs
@@ -9,11 +9,18 @@ public class Player : MonoBehaviour, IStateful {
 //    public KeyCode leftKey = KeyCode.LeftArrow;
 //    public KeyCode rightKey = KeyCode.RightArrow;
     public KeyCode shootKey = KeyCode.Mouse0;
+    public GameObject bulletPrefab;
 
     public float speed = 2f;
     public int ammo = 5;
+    public float bulletSpeed = 5f;
+    public float bulletLifespan = 0.6f; 
 
     private Rigidbody2D body;
+
+    public State GetState() {
+        return null;
+    }
 
     void Awake() {
         body = GetComponent<Rigidbody2D>();
@@ -22,11 +29,27 @@ public class Player : MonoBehaviour, IStateful {
     void Update() {
         var move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         body.velocity = move * speed;
+
+        if (Input.GetKeyDown(shootKey) && CanShoot()) {
+            Debug.Log("Created Bullet!");
+            var bullet = (GameObject)Instantiate(
+                bulletPrefab,
+                gameObject.transform
+            );
+
+            bullet.transform.position = gameObject.transform.position;
+
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 relativeVec = mouseWorldPos - gameObject.transform.position;
+
+            bullet.GetComponent<Rigidbody2D>().velocity = relativeVec.normalized * bulletSpeed;
+            Destroy(bullet, bulletLifespan);
+        }
     }
 
-//    public OnTouchDown(
-
-    public State GetState() {
-        return null;
+    private bool CanShoot() {
+        return true;
     }
+
+
 }
