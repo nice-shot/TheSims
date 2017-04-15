8b5e85ac4e70cdb3c7336606a832d68426af76d9
Nadav Sheffer <shefferu@gmail.com>
2017-04-15 18:53:05 +0300

    Changed player movement to use GetAxis method

--- Assets/Scripts/thesims/TeamZapocalypse/Characters/Player.cs
+++ Assets/Scripts/thesims/TeamZapocalypse/Characters/Player.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using Ai.Goap;
 
 public class Player : MonoBehaviour, IStateful {
-    public KeyCode upKey = KeyCode.UpArrow;
-    public KeyCode downKey = KeyCode.DownArrow;
-    public KeyCode leftKey = KeyCode.LeftArrow;
-    public KeyCode rightKey = KeyCode.RightArrow;
+//    public KeyCode upKey = KeyCode.UpArrow;
+//    public KeyCode downKey = KeyCode.DownArrow;
+//    public KeyCode leftKey = KeyCode.LeftArrow;
+//    public KeyCode rightKey = KeyCode.RightArrow;
     public KeyCode shootKey = KeyCode.Mouse0;
 
     public float speed = 2f;
@@ -20,36 +20,11 @@ public class Player : MonoBehaviour, IStateful {
     }
 
     void Update() {
-        var velocity = body.velocity;
-
-        if (Input.GetKeyUp(upKey) || Input.GetKeyUp(downKey)) {
-            velocity.y = 0;
-        }
-
-        if (Input.GetKeyDown(upKey)) {
-            velocity.y = speed;
-        }
-
-        if (Input.GetKeyDown(downKey)) {
-            velocity.y = -speed;
-        }
-
-        if (Input.GetKeyUp(leftKey) || Input.GetKeyUp(rightKey)) {
-            velocity.x = 0;
-        }
-
-        if (Input.GetKeyDown(rightKey)) {
-            velocity.x = speed;
-        }
-
-        if (Input.GetKeyDown(leftKey)) {
-            velocity.x = -speed;
-        }
-
-
-
-        body.velocity = velocity;
+        var move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        body.velocity = move * speed;
     }
+
+//    public OnTouchDown(
 
     public State GetState() {
         return null;
