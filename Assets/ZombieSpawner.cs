da460431b813cb4a825e9e8ab028c78532f137a9
nice-shot <shefferu@gmail.com>
2017-04-23 12:27:08 +0300

    started adding a zombie spawner - still having problems with its actions

---
+++ Assets/ZombieSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamZapocalypse {
+public class ZombieSpawner : MonoBehaviour {
+    public GameObject zombiePrefab;
+    private int hordeSize = 0;
+
+	// Use this for initialization
+	void Start () {
+        SummonZombies(1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int numOfZombies = GameObject.FindGameObjectsWithTag("Zombie").Length;
+        if (numOfZombies == 0) {
+            hordeSize++;
+            SummonZombies(hordeSize);
+        }
+	}
+
+    void SummonZombies(int numberOfZombies) {
+        Transform[] spawnTransforms = GetComponentsInChildren<Transform>();
+        for (int i=0; i<numberOfZombies; i++) {
+            Transform chosenTransform = spawnTransforms[Random.Range(0, spawnTransforms.Length)];
+            GameObject zombie = (GameObject)Instantiate(zombiePrefab);
+            var pos = chosenTransform.position;
+            // Z position for characters is always -2
+            zombie.transform.position = new Vector3(pos.x, pos.y, -2);
+        }
+    }
+}
+}
