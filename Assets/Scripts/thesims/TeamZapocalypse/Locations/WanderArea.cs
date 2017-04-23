8e36139f92f2a209b7e8b9a530c7981e05b15732
nice-shot <shefferu@gmail.com>
2017-04-23 11:59:17 +0300

    removed unnecessary comment form wander area

--- Assets/Scripts/thesims/TeamZapocalypse/Locations/WanderArea.cs
+++ Assets/Scripts/thesims/TeamZapocalypse/Locations/WanderArea.cs
@@ -4,7 +4,6 @@ using Ai.Goap;
 namespace TeamZapocalypse {
 public class WanderArea : MonoBehaviour, IStateful {
     public State GetState() {
-        // TODO: Add limited resources
         return null;
     }
 }
