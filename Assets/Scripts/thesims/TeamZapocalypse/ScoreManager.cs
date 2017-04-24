d87cb7fcf98d3765de2d258dc9cd33a8f6c39abf
nice-shot <shefferu@gmail.com>
2017-04-24 16:56:23 +0300

    Added survival time! This is a real game now

---
+++ Assets/Scripts/thesims/TeamZapocalypse/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    public Text scoreText;
+    private int score;
+    private int increaseScoreEvery = 1;
+    private float previousTime = 0;
+
+	void Start () {
+        score = 0;
+	}
+
+    void UpdateScore () {
+        scoreText.text = "Survival time: " + score.ToString();
+    }
+	
+	void Update () {
+        if (Time.time - previousTime > increaseScoreEvery) {
+            score++;
+            UpdateScore();
+            previousTime = Time.time;
+        }
+	}
+}
